dc7181e7a278e25a62c190d157757562ed98272e
kewalll <kewal210504@gmail.com>
2024-11-06 16:33:22 +0530

    update user info

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,49 +1,68 @@
 import { useSelector } from "react-redux";
-import { TextInput, Button } from "flowbite-react";
-import { useState, useRef } from "react";
-import { useEffect } from "react";
+import { TextInput, Button, Alert } from "flowbite-react";
+import { useState } from "react";
+import {
+  updateStart,
+  updateSuccess,
+  updateFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
+  signoutSuccess,
+} from '../redux/user/userSlice';
+import { useDispatch } from "react-redux";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
-  const [imageFile, setImageFile] = useState(null);
-  const [imageFileUrl, setImageFileUrl] = useState(null);
-  const filePickerRef = useRef();
+  const [formData, setFormData] = useState({});
+  const dispatch = useDispatch();
+  const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+  const [updateUserError, setUpdateUserError] = useState(null);
 
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      setImageFile(file);
-      setImageFileUrl(URL.createObjectURL(file));
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]:e.target.value})
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setUpdateUserError(null);
+    setUpdateUserSuccess(null);
+    if (Object.keys(formData).length === 0) {
+      setUpdateUserError('No changes made');
+      return;
     }
-  };
-
-  useEffect(() => {
-    if (imageFile) {
-      uploadImage();
+    
+    try {
+      dispatch(updateStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(updateFailure(data.message));
+        setUpdateUserError(data.message);
+      } else {
+        dispatch(updateSuccess(data));
+        setUpdateUserSuccess("User's profile updated successfully");
+      }
+    } catch (error) {
+      dispatch(updateFailure(error.message));
+      setUpdateUserError(error.message);
     }
-  }, [imageFile]);
-
-  const uploadImage = async () => {
-    console.log("uploading image...");
-  };
-
+  }
+  
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
-      <form className="flex flex-col gap-4">
-        <input
-          type="file"
-          accept="image/*"
-          onChange={handleImageChange}
-          ref={filePickerRef}
-          hidden
-        />
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <div
           className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
-          onClick={() => filePickerRef.current.click()}
         >
           <img
-            src={imageFileUrl || currentUser.profilePicture}
+            src={currentUser.profilePicture}
             alt="user"
             className="rounded-full w-full h-full object-cover"
           />
@@ -53,29 +72,39 @@ export default function DashProfile() {
           id="username"
           placeholder="username"
           defaultValue={currentUser.username}
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <TextInput
           type="email"
           id="email"
           placeholder="email"
           defaultValue={currentUser.email}
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <TextInput
           type="password"
           id="password"
           placeholder="password"
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <Button type="submit" gradientDuoTone="purpleToBlue" outline>
-          updates
+          Update
         </Button>
       </form>
       <div className="text-red-500 flex justify-between mt-5">
         <span className="cursor-pointer">Delete Account</span>
         <span className="cursor-pointer">Sign Out</span>
       </div>
+      {updateUserSuccess && (
+        <Alert color='success' className='mt-5'>
+          {updateUserSuccess}
+        </Alert>
+      )}
+      {updateUserError && (
+        <Alert color='failure' className='mt-5'>
+          {updateUserError}
+        </Alert>
+      )}
     </div>
   );
 }
